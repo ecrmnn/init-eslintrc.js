95d2c15cc1c8749331c5c244582bdac342b0b147
Daniel Eckermann <danieleckermann@gmail.com>
2018-01-29 22:10:18 +0100

    Adds no-console and no-ternary

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-printf "module.exports = {\n  extends: 'airbnb-base',\n  parserOptions: {\n    sourceType: 'script',\n  },\n  plugins: [\n    'import',\n  ],\n  rules: {\n    'newline-before-return': 2,\n  },\n};\n" > .eslintrc.js && [ -f package.json ] || echo {} > package.json && npm install eslint eslint-config-airbnb-base eslint-plugin-import -D
+printf "module.exports = {\n  extends: 'airbnb-base',\n  parserOptions: {\n    sourceType: 'script',\n  },\n  plugins: [\n    'import',\n  ],\n  rules: {\n    'newline-before-return': 'error',\n    'no-console': 'error',\n    'no-ternary': 'error',\n  },\n};\n" > .eslintrc.js && [ -f package.json ] || echo {} > package.json && npm install eslint eslint-config-airbnb-base eslint-plugin-import -D
